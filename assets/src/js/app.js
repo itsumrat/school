00bbc6b14470b265b942a244b7059eaf8f43171e
itsumrat <itsumrat@gmail.com>
2017-01-08 15:36:02 +0600

    teacher list new

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -7,59 +7,109 @@ jQuery(document).ready(function($){
 	$('#achiever-carousel').carousel({wrap:true});
 
 	//Teacher List
-	$('td', 'table').each(function(i) {
-	    $(this).text(i+1);
-	});
-
-	$('table.paginated').each(function() {
-	    var currentPage = 0;
-	    var numPerPage = 5;
-	    var $table = $(this);
-	    $table.bind('repaginate', function() {
-	        $table.find('tbody tr').hide().slice(currentPage * numPerPage, (currentPage + 1) * numPerPage).show();
-	    });
-	    $table.trigger('repaginate');
-	    var numRows = $table.find('tbody tr').length;
-	    var numPages = Math.ceil(numRows / numPerPage);
-	    var $pager = $('<div class="pager"></div>');
-	    var $previous = $('<span class="previous"><<</spnan>');
-	    var $next = $('<span class="next">>></spnan>');
-	    for (var page = 0; page < numPages; page++) {
-	        $('<span class="page-number"></span>').text(page + 1).bind('click', {
-	            newPage: page
-	        }, function(event) {
-	            currentPage = event.data['newPage'];
-	            $table.trigger('repaginate');
-	            $(this).addClass('active').siblings().removeClass('active');
-	        }).appendTo($pager).addClass('clickable');
-	    }
-	    $pager.insertBefore($table).find('span.page-number:first').addClass('active');
-	    $previous.insertBefore('span.page-number:first');
-	    $next.insertAfter('span.page-number:last');
+	$.fn.pageMe = function(opts){
+	    var $this = this,
+	        defaults = {
+	            perPage: 7,
+	            showPrevNext: false,
+	            hidePageNumbers: false
+	        },
+	        settings = $.extend(defaults, opts);
 	    
-	    $next.click(function (e) {
-	        $previous.addClass('clickable');
-	        $pager.find('.active').next('.page-number.clickable').click();
+	    var listElement = $this;
+	    var perPage = settings.perPage; 
+	    var children = listElement.children();
+	    var pager = $('.pager');
+	    
+	    if (typeof settings.childSelector!="undefined") {
+	        children = listElement.find(settings.childSelector);
+	    }
+	    
+	    if (typeof settings.pagerSelector!="undefined") {
+	        pager = $(settings.pagerSelector);
+	    }
+	    
+	    var numItems = children.size();
+	    var numPages = Math.ceil(numItems/perPage);
+
+	    pager.data("curr",0);
+	    
+	    if (settings.showPrevNext){
+	        $('<li><a href="#" class="prev_link">«</a></li>').appendTo(pager);
+	    }
+	    
+	    var curr = 0;
+	    while(numPages > curr && (settings.hidePageNumbers==false)){
+	        $('<li><a href="#" class="page_link">'+(curr+1)+'</a></li>').appendTo(pager);
+	        curr++;
+	    }
+	    
+	    if (settings.showPrevNext){
+	        $('<li><a href="#" class="next_link">»</a></li>').appendTo(pager);
+	    }
+	    
+	    pager.find('.page_link:first').addClass('active');
+	    pager.find('.prev_link').hide();
+	    if (numPages<=1) {
+	        pager.find('.next_link').hide();
+	    }
+	      pager.children().eq(1).addClass("active");
+	    
+	    children.hide();
+	    children.slice(0, perPage).show();
+	    
+	    pager.find('li .page_link').click(function(){
+	        var clickedPage = $(this).html().valueOf()-1;
+	        goTo(clickedPage,perPage);
+	        return false;
 	    });
-	    $previous.click(function (e) {
-	        $next.addClass('clickable');
-	        $pager.find('.active').prev('.page-number.clickable').click();
+	    pager.find('li .prev_link').click(function(){
+	        previous();
+	        return false;
 	    });
-	    $table.on('repaginate', function () {
-	        $next.addClass('clickable');
-	        $previous.addClass('clickable');
+	    pager.find('li .next_link').click(function(){
+	        next();
+	        return false;
+	    });
+	    
+	    function previous(){
+	        var goToPage = parseInt(pager.data("curr")) - 1;
+	        goTo(goToPage);
+	    }
+	     
+	    function next(){
+	        goToPage = parseInt(pager.data("curr")) + 1;
+	        goTo(goToPage);
+	    }
+	    
+	    function goTo(page){
+	        var startAt = page * perPage,
+	            endOn = startAt + perPage;
 	        
-	        setTimeout(function () {
-	            var $active = $pager.find('.page-number.active');
-	            if ($active.next('.page-number.clickable').length === 0) {
-	                $next.removeClass('clickable');
-	            } else if ($active.prev('.page-number.clickable').length === 0) {
-	                $previous.removeClass('clickable');
-	            }
-	        });
-	    });
-	    $table.trigger('repaginate');
-	});
+	        children.css('display','none').slice(startAt, endOn).show();
+	        
+	        if (page>=1) {
+	            pager.find('.prev_link').show();
+	        }
+	        else {
+	            pager.find('.prev_link').hide();
+	        }
+	        
+	        if (page<(numPages-1)) {
+	            pager.find('.next_link').show();
+	        }
+	        else {
+	            pager.find('.next_link').hide();
+	        }
+	        
+	        pager.data("curr",page);
+	      	pager.children().removeClass("active");
+	        pager.children().eq(page+1).addClass("active");
+	    
+	    }
+	};
+    
+    $('#myTable').pageMe({pagerSelector:'#myPager',showPrevNext:true,hidePageNumbers:false,perPage:10});
 
 
 	// Remove carousel caption in smaller screen
